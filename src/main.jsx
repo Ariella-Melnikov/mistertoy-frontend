9c86fc90cf8cd00b36392e09993bec9ca16d5a23
Ariella Melnikov <melnikov.ariella@gmail.com>
2024-07-11 19:03:58 +0300

    Add the base of SCSS for styling the app

--- src/main.jsx
+++ src/main.jsx
@@ -3,4 +3,6 @@ import ReactDOM from 'react-dom/client'
 import { App } from './root-cmp.jsx'
 import './i18n.js'
 
+import './styles/main.scss'
+
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
