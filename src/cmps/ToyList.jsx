b63fe63e23d4b24e8931f43c729bd8ca73a72cf6
Ariella Melnikov <melnikov.ariella@gmail.com>
2024-07-15 19:09:09 +0300

    update toy list with user options

--- src/cmps/ToyList.jsx
+++ src/cmps/ToyList.jsx
@@ -1,8 +1,9 @@
 import { Link } from "react-router-dom"
 
 import { ToyPreview } from "./ToyPreview.jsx"
+import { CardToyPreview } from "./CardToyPreview.jsx"
 
-export function ToyList({ toys, onRemoveToy, onToggleToy }) {
+export function ToyList({ toys, user, onRemoveToy }) {
 
     if (!toys.length) return <p>No toys to show...</p>
 
@@ -10,11 +11,11 @@ export function ToyList({ toys, onRemoveToy, onToggleToy }) {
         <ul className="toy-list">
             {toys.map(toy =>
                 <li key={toy._id}>
-                    <ToyPreview toy={toy} onToggleToy={()=>onToggleToy(toy)} />
+                    <CardToyPreview toy={toy} user={user}  onRemoveToy={onRemoveToy} />
                     <section>
-                        <button onClick={() => onRemoveToy(toy._id)}>Remove</button>
+                        {/* <button onClick={() => onRemoveToy(toy._id)}>Remove</button> */}
                         {/* <button><Link to={`/toy/${toy._id}`}>Details</Link></button> */}
-                        <button><Link to={`/toy/edit/${toy._id}`}>Edit</Link></button>
+                        {/* <button><Link to={`/toy/edit/${toy._id}`}>Edit</Link></button> */}
                     </section>
                 </li>
             )}
